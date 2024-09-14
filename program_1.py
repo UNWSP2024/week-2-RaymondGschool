aebab95d612acf13d8262bafe340a3fa253cf177
RaymondGschool <2025grotr66@moundsviewschools.org>
2024-09-13 22:05:30 -0500

    FinishedAssignment

--- program_1.py
+++ program_1.py
@@ -1,5 +1,11 @@
 def personal_information():
-    print('...')
+    """
+    Prints personal details to console
+    """
+    print('Raymond Grottodden')
+    print('561 Lake Ridge Court, Shoreview, MN 55126')
+    print('651-347-0594')
+    print('Engineering')
 
 
 # Line which calls the above function.
